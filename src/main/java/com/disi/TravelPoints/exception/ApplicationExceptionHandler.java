dc8c3584148f7b9d9ebf42f9bd7429193e080104
Bogdan Matioc <bogdanmatioc@gmail.com>
2023-04-14 14:53:02 +0300

    add cors configuration

--- src/main/java/com/disi/TravelPoints/exception/ApplicationExceptionHandler.java
+++ src/main/java/com/disi/TravelPoints/exception/ApplicationExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.disi.TravelPoints.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
 import java.util.Map;
 
 @ControllerAdvice
 public class ApplicationExceptionHandler {
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<Map<String, String>> handleException(CustomException exception)  {
-        var errorResponse = Map.of("message", exception.getMessage());
+    public ResponseEntity<Map<String, Object>> handleException(CustomException exception)  {
+        Map<String, Object> errorResponse = Map.of(
+                "message", exception.getMessage(),
+                "status", exception.getStatus().value()
+        );
 
-        return new ResponseEntity<>(errorResponse, exception.getStatus());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
