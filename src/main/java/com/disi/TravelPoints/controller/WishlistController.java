cd76ea3271ff21d963dd8f8dbf9b11e1ddf49b79
RazvanMadar <razvanmadar@gmail.com>
2023-04-21 13:38:28 +0300

    Added landmark in wishlist

--- src/main/java/com/disi/TravelPoints/controller/WishlistController.java
+++ src/main/java/com/disi/TravelPoints/controller/WishlistController.java
@@ -6,10 +6,7 @@ import com.disi.TravelPoints.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,19 @@ public class WishlistController {
     public ResponseEntity<List<LandmarkDetails>> getWishlistForUser(@PathVariable Long id) throws CustomException {
         try {
             return ResponseEntity.ok(wishlistService.getWishlistForUser(id));
+        } catch (Exception exception) {
+            throw CustomException
+                    .builder()
+                    .status(HttpStatus.NOT_FOUND)
+                    .message(exception.getMessage())
+                    .build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> addLandmarkInWishlist(@RequestParam Long landmarkId, @RequestParam Long userId) throws CustomException {
+        try {
+            return new ResponseEntity<>(wishlistService.addLandmarkInWishlist(landmarkId, userId), HttpStatus.CREATED);
         } catch (Exception exception) {
             throw CustomException
                     .builder()
--- src/main/java/com/disi/TravelPoints/service/WishlistService.java
+++ src/main/java/com/disi/TravelPoints/service/WishlistService.java
@@ -4,19 +4,26 @@ import com.disi.TravelPoints.dto.LandmarkDetails;
 import com.disi.TravelPoints.dto.UserDetails;
 import com.disi.TravelPoints.exception.CustomException;
 import com.disi.TravelPoints.model.Landmark;
+import com.disi.TravelPoints.model.User;
 import com.disi.TravelPoints.model.Wishlist;
+import com.disi.TravelPoints.repository.LandmarkRepository;
+import com.disi.TravelPoints.repository.UserRepository;
 import com.disi.TravelPoints.repository.WishlistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class WishlistService {
     private final WishlistRepository wishlistRepository;
+    private final UserRepository userRepository;
+    private final LandmarkRepository landmarkRepository;
 
     public List<LandmarkDetails> getWishlistForUser(Long id) throws Exception {
         Wishlist wishlist = wishlistRepository.findByUser_Id(id)
@@ -25,6 +32,27 @@ public class WishlistService {
         return wishlist.getLandmarks().stream()
                 .map(this::mapLandmarkToLandmarkDetails)
                 .collect(Collectors.toList());
+    }
+
+    public Long addLandmarkInWishlist(Long landmarkId, Long userId) throws Exception {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new Exception("User not found!"));
+        Landmark landmark = landmarkRepository.findById(landmarkId)
+                .orElseThrow(() -> new Exception("Landmark not found!"));
+
+        Optional<Wishlist> wishlist = wishlistRepository.findByUser_Id(userId);
+        if (wishlist.isEmpty()) {
+            Wishlist newWishlist = new Wishlist();
+            newWishlist.setUser(user);
+            newWishlist.setLandmarks(Set.of(landmark));
+            return wishlistRepository.save(newWishlist).getId();
+        }
+
+        Wishlist actualWishlist = wishlist.get();
+        final Set<Landmark> landmarks = actualWishlist.getLandmarks();
+        landmarks.add(landmark);
+        actualWishlist.setLandmarks(landmarks);
+        return actualWishlist.getId();
     }
 
     private LandmarkDetails mapLandmarkToLandmarkDetails(Landmark landmark) {
