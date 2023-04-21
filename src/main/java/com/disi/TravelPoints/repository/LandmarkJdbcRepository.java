8b9d823ddbf443b7916ca722e05b477a53d54ed7
RazvanMadar <razvanmadar@gmail.com>
2023-04-21 16:35:08 +0300

    Filtered landmarks by category or location + deleted previous implementation for get landmarks

---
+++ src/main/java/com/disi/TravelPoints/repository/LandmarkJdbcRepository.java
@@ -0,0 +1,52 @@
+package com.disi.TravelPoints.repository;
+
+import com.disi.TravelPoints.dto.LandmarkDetails;
+import lombok.AllArgsConstructor;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+@Repository
+@AllArgsConstructor
+public class LandmarkJdbcRepository {
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public List<LandmarkDetails> getLandmarksByCategoryAndLocation(String category, String location) {
+        MapSqlParameterSource parameters = new MapSqlParameterSource();
+        boolean isFirstCondition = false;
+        StringBuilder query = new StringBuilder("SELECT * FROM landmarks ");
+        if (category != null) {
+            query.append("WHERE category = " + "'").append(category).append("' ");
+            isFirstCondition = true;
+        }
+        if (location != null) {
+            if (!isFirstCondition) {
+                query.append("WHERE ");
+            }
+            else {
+                query.append("AND ");
+            }
+            query.append("location = " + "'").append(location).append("'");
+        }
+
+        return jdbcTemplate.query(query.toString(), parameters, (rs, rowNum) -> mapLandmarkToLandmarkDetails(rs));
+    }
+
+    private LandmarkDetails mapLandmarkToLandmarkDetails(ResultSet rs) throws SQLException {
+        return LandmarkDetails
+                .builder()
+                .id(rs.getLong("id"))
+                .name(rs.getString("name"))
+                .location(rs.getString("location"))
+                .image(rs.getBytes("image"))
+                .textDescription(rs.getString("text_description"))
+                .audioDescription(rs.getBytes("audio_description"))
+                .price(rs.getFloat("price"))
+                .category(rs.getString("category"))
+                .build();
+    }
+}
