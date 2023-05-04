6e765d50dfc60cd80e666bf06f2dabfd556fee9b
andreea <andreeat7@yahoo.com>
2023-05-05 01:26:53 +0300

    refactor

---
+++ src/main/java/com/disi/TravelPoints/service/EmailService.java
@@ -0,0 +1,38 @@
+package com.disi.TravelPoints.service;
+
+import com.disi.TravelPoints.Email.Email;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    public void sendEmail(Email message) throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("tatar.andreea23@gmail.com", "cqqfxoiutrladjcy");
+            }
+        });
+
+        Message emailMessage = new MimeMessage(session);
+        emailMessage.setFrom(new InternetAddress(message.getFrom()));
+        emailMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(message.getTo()));
+        emailMessage.setSubject(message.getSubject());
+        emailMessage.setText(message.getBody());
+
+        Transport.send(emailMessage);
+    }
+}
